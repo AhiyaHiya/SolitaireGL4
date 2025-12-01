9b8ec23e04731be712c4c7ef6c0c40e1a24343bd
Jaime O. Rios <3794310+AhiyaHiya@users.noreply.github.com>
2025-12-01 13:29:14 -0700

    Add CPM.cmake, update existing CMake, add stb and json

--- CMake/glad.cmake
+++ CMake/glad.cmake
@@ -1,18 +1,12 @@
-include(FetchContent)
+CPMAddPackage("gh:Dav1dde/glad#v2.0.6")
 
-FetchContent_Declare(
-    glad
-    GIT_REPOSITORY https://github.com/Dav1dde/glad
-    GIT_TAG        v2.0.6
-    GIT_SHALLOW    TRUE
-    GIT_PROGRESS   TRUE
+add_subdirectory("${glad_SOURCE_DIR}/cmake" glad_cmake)
+glad_add_library(glad
+    REPRODUCIBLE
+    EXCLUDE_FROM_ALL
+    LOADER
+    API gl:core=4.6
+    EXTENSIONS
+        GL_ARB_bindless_texture
+        GL_EXT_texture_compression_s3tc
 )
-
-FetchContent_GetProperties(glad)
-if(NOT glad_POPULATED)
-    message("Fetching glad")
-    FetchContent_MakeAvailable(glad)
-
-    add_subdirectory("${glad_SOURCE_DIR}/cmake" glad_cmake)
-    glad_add_library(glad REPRODUCIBLE EXCLUDE_FROM_ALL LOADER API gl:core=4.6 EXTENSIONS GL_ARB_bindless_texture GL_EXT_texture_compression_s3tc)
-endif()
