5f4d92409f34ef372c1f00c20500b0243e51f1ec
Jaime O. Rios <3794310+AhiyaHiya@users.noreply.github.com>
2025-12-05 15:51:04 -0700

    Move types from window.hpp to types.hpp

---
+++ Game/types.hpp
@@ -0,0 +1,10 @@
+#ifndef _GAME_TYPES_HPP__
+#define _GAME_TYPES_HPP__
+
+#include <cstdint>
+#include <string>
+
+using error_code_t = std::int32_t;
+using error_message_t = std::string;
+
+#endif // _GAME_TYPES_HPP__
--- Game/window.hpp
+++ Game/window.hpp
@@ -13,9 +13,6 @@
 #include <memory>
 #include <string_view>
 
-using error_code_t = std::int32_t;
-using error_message_t = std::string;
-
 struct GLFWwindow;
 
 auto create_window(std::int32_t width, std::int32_t height, std::string_view title) 
